4b1c39fe361ab7a165081208a7b463184696a74b
mengxinzxz <2223529500@qq.com>
2023-12-01 23:10:48 +0800

    熏鱼↓调

--- extension.js
+++ extension.js
@@ -45619,7 +45619,7 @@ player.die().set('source',trigger.player);
 event.finish();
 return;
 }
-trigger.player.chooseCard('交给'+get.translation(player)+'三张牌以示对其的关爱',true,'he',3,(card,player)=>get.name(card).indexOf('bilibili_duoyang_')==-1);
+trigger.player.chooseCard('交给'+get.translation(player)+'两张牌以示对其的关爱',true,'he',2,(card,player)=>get.name(card).indexOf('bilibili_duoyang_')==-1);
 'step 1'
 if(result.bool){
 trigger.player.give(result.cards,player);
@@ -48534,7 +48534,7 @@ bilibili_shengxunyu:'生熏鱼',
 bilibili_jinyan:'禁言',
 bilibili_jinyan_info:'锁定技，其他角色于你的回合内至多成为一次你使用非【奇正相生】牌的目标。',
 bilibili_hehu:'呵护',
-bilibili_hehu_info:'锁定技，其他角色回合结束时，其须交给你三张牌，然后其摸两张牌并获得两张【影】（这些牌不计入手牌上限且不能对你使用）。',
+bilibili_hehu_info:'锁定技，其他角色回合结束时，其须交给你两张牌，然后其摸两张牌并获得两张【影】（这些牌不计入手牌上限且不能对你使用）。',
 bilibili_yutai:'彧态',
 bilibili_yutai_info:'你可以将X张牌当作【奇正相生】使用（X为你本回合发动〖彧态〗的次数+1，且X至多为3）。',
 bilibili_yutai_append:'<span style="font-family:yuanli">我是活动群团宠，我最爱的就是惹事然后被宵禁</span>',
