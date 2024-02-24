3c3f92f84b1cc69bf57be16bd5fcc89692f37f3f
Howard <howardzhow@126.com>
2024-02-24 11:22:52 +0800

    修正HD_REname算法使得1.10.7以上版本不再报错

--- extension.js
+++ extension.js
@@ -1366,7 +1366,7 @@ var list3=['卧龙凤雏'];//不修改名称的translate白名单
 [['张机','张仲景'],['蔡琰','蔡文姬'],['卧龙','卧龙诸葛'],
 ['严虎','严白虎'],['甄宓','甄姬'],['伏寿','伏皇后'],
 ['吉本','吉平']].forEach(name=>{
-list.filter(name2=>!list2.includes(name2)&&!lib.translate[name2].includes(list3)&&lib.translate[name2].includes(name[0])).forEach(name3=>{
+list.filter(name2=>!list2.includes(name2)&&lib.translate[name2]&&list3.filter(name4=>lib.translate[name2].includes(name4)).length==0&&lib.translate[name2].includes(name[0])).forEach(name3=>{
 var str=lib.translate[name3];
 var num=str.indexOf(name[0]);
 lib.translate[name3]=str.slice(0,num)+name[1]+str.slice(num+name[0].length,str.length);
