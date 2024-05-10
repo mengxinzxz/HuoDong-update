4cf682586975d86c9026013b38ff644cd9385476
mengxinzxz <2223529500@qq.com>
2024-05-11 06:08:02 +0800

    删除改动（实测

--- extension.js
+++ extension.js
@@ -2013,16 +2013,7 @@ lib.skill.yinka.trigger={global:['drawBegin','judgeBegin']};
 lib.skill.yinka.firstDo=true;
 lib.skill.yinka.group='yinka_view';
 lib.skill.yinka.subSkill={view:{ai:{viewHandcard:true,skillTagFilter:(player,arg,target)=>target!=player}}};
-//李丰
-lib.skill.dctunchu.trigger={global:'gameDrawEnd'};
-lib.skill.dctunchu.filter=function(event,player){
-return player.countCards('h')<game.players.slice().concat(game.dead).length*4;
-};
-lib.skill.dctunchu.content=async function(event,trigger,player){
-await player.gain(get.cards(game.players.slice().concat(game.dead).length*4-player.countCards('h')),'draw');
-player._start_cards=player.getCards('h');
-};
-lib.translate.dctunchu_info='锁定技。①分发起始手牌后，若你的手牌数小于X，则你将起始手牌数调整为X（X为游戏人数的四倍）。②你的手牌不能被弃置。③准备阶段，若你的手牌数大于你的体力上限，则你本回合至多使用三张牌。';
+
 
 //precT
 //翻译
