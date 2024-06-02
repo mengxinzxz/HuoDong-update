4e83a21210b2e2fdd8178c7e7e90accd6cdbb2ba
xizifu <2238983941@qq.com>
2024-06-03 01:54:58 +0800

    微信界陆逊2.0

--- extension.js
+++ extension.js
@@ -37257,7 +37257,7 @@ wechat_sunluban:['female','wu',3,['wechatchanhui','wechatjiaojin'],['die_audio:x
 wechat_wuguotai:['female','wu',3,['wechatganlu','buyi'],[]],
 wechat_liubiao:['male','qun',3,['wechatrezishou','wechatzongshi'],[]],
 wechat_liuchen:['male','shu',4,['zhanjue','wechatqinwang'],[]],
-wechat_luxun:['male','wu',3,['wechatqianxun','lianying'],[]],
+wechat_luxun:['male','wu',3,['wechatreqianxun','lianying'],[]],
 wechat_pangtong:['male','shu',3,['lianhuan','wechatniepan','wechathuzhu'],[]],
 wechat_zhangxingcai:['female','shu',3,['wechatshenxian','wechatqiangwu'],[]],
 wechat_zuoci:['male','qun',3,['wechatyigui','wechatshendao'],[]],
@@ -38568,6 +38568,71 @@ trigger.cancel();
 },
 },
 wechatqianxun:{
+audio:'reqianxun',
+hiddenCard:function(player,name){
+var card={name:name,isCard:true};
+var info=get.info(card,false);
+return get.type(name)=='trick'&&player.countCards('h')==1&&(!info.notarget&&(info.toself||info.singleCard||!info.selectTarget||info.selectTarget==1));
+},
+enable:'chooseToUse',
+filter:function(event,player){
+if(event.type=='wuxie') return false;
+var cards=player.getCards('h');
+if(cards.length!=1) return false;
+var mod2=game.checkMod(cards[0],player,'unchanged','cardEnabled2',player);
+if(mod2===false) return false;
+for(var i of lib.inpile){
+var card={name:i,isCard:true};
+var info=get.info(card,false);
+if((!info.notarget&&(info.toself||info.singleCard||!info.selectTarget||info.selectTarget==1))&&get.type(i)=='trick'&&event.filterCard(get.autoViewAs({name:i},cards),player,event)) return true;
+}
+return false;
+},
+usable:1,
+chooseButton:{
+dialog:function(event,player){
+var cards=player.getCards('h');
+var list=[];
+for(var i of lib.inpile){
+var card={name:i,isCard:true};
+var info=get.info(card,false);
+if((!info.notarget&&(info.toself||info.singleCard||!info.selectTarget||info.selectTarget==1))&&get.type(i)=='trick'&&event.filterCard(get.autoViewAs({name:i},cards),player,event)){
+list.push(['锦囊','',i]);
+}
+}
+return ui.create.dialog('谦逊',[list,'vcard'],'hidden');
+},
+check:function(button){
+var player=_status.event.player;
+return player.getUseValue({name:button.link[2]})+1;
+},
+backup:function(links,player){
+return {
+audio:'reqianxun',
+popname:true,
+filterCard:true,
+selectCard:-1,
+position:'h',
+viewAs:{
+name:links[0][2],
+},
+}
+},
+prompt:function(links,player){
+return '将'+get.translation(player.getCards('h')[0])+'当作'+get.translation(links[0][2])+'使用';
+},
+},
+subSkill:{
+backup:{audio:'reqianxun'},
+},
+ai:{
+order:12,
+result:{
+player:1,
+},
+},
+},
+wechatreqianxun:{
     audio:'reqianxun',
     inherit:'reqianxun',
     async cost(event,trigger,player){
@@ -38578,19 +38643,19 @@ wechatqianxun:{
         player.addSkill(event.name+'2');
     },
 },
-wechatqianxun2:{
+wechatreqianxun2:{
     charlotte:true,
     audio:'reqianxun',
     trigger:{global:'phaseEnd'},
     forced:true,
     content:function(){
-        var cards=player.getExpansions('wechatqianxun2');
+        var cards=player.getExpansions('wechatreqianxun2');
         if(cards.length) player.gain(cards,'draw');
-        player.removeSkill('wechatqianxun2');
+        player.removeSkill('wechatreqianxun2');
     },
     intro:{
         mark:function(dialog,storage,player){
-            var cards=player.getExpansions('wechatqianxun2');
+            var cards=player.getExpansions('wechatreqianxun2');
             if(player.isUnderControl(true)) dialog.addAuto(cards);
             else return '共有'+get.cnNumber(cards.length)+'张牌';
         },
@@ -43609,7 +43674,9 @@ wechatqinwang1:'勤王',
 wechatqinwang_info:'当你需要打出【杀】时，你可以选择一名其他角色，其可以替你打出一张【杀】。',
 wechat_luxun:'微信陆逊',
 wechatqianxun:'谦逊',
-wechatqianxun_info:'每当一张延时类锦囊牌或其他角色使用的普通锦囊牌生效时，若你是此牌的唯一目标，你可以将任意张手牌置于你的武将牌上，若如此做，此回合结束时，你获得你武将牌上的所有牌。',
+wechatqianxun_info:'每名角色的回合限一次，若你的手牌数为1，你可以将所有手牌当作一张单体非延时锦囊牌使用。',
+wechatreqianxun:'谦逊',
+wechatreqianxun_info:'每当一张延时类锦囊牌或其他角色使用的普通锦囊牌生效时，若你是此牌的唯一目标，你可以将任意张手牌置于你的武将牌上，若如此做，此回合结束时，你获得你武将牌上的所有牌。',
 wechat_pangtong:'微信庞统',
 wechatniepan:'涅槃',
 wechatniepan_info:'限定技，当你处于濒死状态时，你可以弃置区域内的所有牌，复原武将牌，摸三张牌并将体力值回复至3，然后本局游戏你造成的伤害均视为火属性。',
