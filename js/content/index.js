e1e794a6850e6b562309b5ea3caab53a70ff8f05
mengxinzxz <2223529500@qq.com>
2024-07-08 14:10:46 +0800

    1

--- js/content/index.js
+++ js/content/index.js
@@ -1,57 +1,6 @@
 import { lib, game, ui, get, ai, _status } from '../../../../noname.js'
 export function content(config, pack) {
 	//更新公告
-	game.bolShowNewPack = function () {
-		//更新告示
-		var HuoDong_update = [
-			'/setPlayer/',
-			'bugfix',
-			'感谢@xizifu提交的Pull Request',
-			'对活动武将的js进行拆分（后续可能会继续拆分）',
-			'添加微信杀武将：极孙策、极荀彧、极甄宓、曹彰、周不疑、李通、鲍三娘、徐氏',
-			'添加欢杀武将：阎柔、严白虎、董卓、喵吕玲绮',
-			'自嗨包狂神添加7月2日使用权',
-			'To be continued...',
-		];
-		//更新武将
-		var HuoDong_players = [
-			'Mbaby_yanrou', 'wechat_sunce', 'wechat_xunyu', 'wechat_zhenji', 'wechat_caozhang', 'wechat_yj_zhoubuyi',
-			'wechat_litong', 'wechat_baosanniang', 'wechat_xushi', 'Mbaby_yanbaihu', 'Mbaby_dongzhuo', 'Mmiao_lvlingqi',
-		];
-		//加载
-		var dialog = ui.create.dialog(
-			'<span class="text center">' +
-			'新人制作扩展，希望大家支持<br>新人技术不足，希望大家包涵' +
-			'<br>' +
-			'<a href="https://github.com/mengxinzxz/HuoDong-update.git">点击前往活动武将Github仓库</a>' +
-			'<br>' +
-			'活动武将 ' + lib.extensionPack.活动武将.version + ' 更新内容' +
-			'</span>', 'hidden');
-		for (var i = 0; i < HuoDong_update.length; i++) {
-			if (HuoDong_update[i] == '/setPlayer/') {
-				if (HuoDong_players.length) dialog.addSmall([HuoDong_players, 'character']);
-			}
-			else {
-				var li = document.createElement('li');
-				li.innerHTML = HuoDong_update[i];
-				li.style.textAlign = 'left';
-				dialog.content.appendChild(li);
-			}
-		}
-		dialog.open();
-		var hidden = false;
-		if (!ui.auto.classList.contains('hidden')) {
-			ui.auto.hide();
-			hidden = true;
-		}
-		game.pause();
-		var control = ui.create.control('确定', function () {
-			dialog.close();
-			control.close();
-			if (hidden) ui.auto.show();
-			game.resume();
-		});
-	};
 	var version = lib.config.extension_活动武将_HDversion;
 	if (!version || version != lib.extensionPack.活动武将.version) {
 		lib.game.showChangeLog = function () {
