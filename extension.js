0b253664736c6c2700633f8a5089ce60f9f60e07
MengXinZXZ <2223529500@qq.com>
2025-05-20 23:10:08 +0800

    为活动武将添加info.json文件，作者名添加后续维护作者
    孩子们，我要肘了
    
    Co-Authored-By: xizifu <165245042+xizifu@users.noreply.github.com>

--- extension.js
+++ extension.js
@@ -75,24 +75,25 @@ let extensionPackage = {
 	precontent: precontent,
 	config: config,
 	help: help,
-	package: {
-		intro: '新人制作扩展，希望大家支持。' +
-			'<br>新人技术不足，希望大家包涵。' +
-			'<br><a href="https://github.com/HuoDong-Update-Organization/HuoDong-update">点击前往活动武将Github仓库</a>' +
-			'<br><li>欢迎大家进群支持活动武将' +
-			//入群二维码图片
-			'<br><img style=width:238px src=' + lib.assetURL + 'extension/活动武将/HuoDong_QQ.png>' +
-			'',
-		author: '萌新（转型中）',
-		diskURL: '',
-		forumURL: '',
-		version: '0.3.8 - 待定',
-		//新人制作扩展，希望大家支持。
-		//新人技术不足，希望大家包涵。
-		//壹、贰、叁、肆、伍、陆、柒、捌、玖、拾
-	},
-	files: {}
+	files: {},
+	//新人制作扩展，希望大家支持。
+	//新人技术不足，希望大家包涵。
+	//壹、贰、叁、肆、伍、陆、柒、捌、玖、拾
 };
 
 export let type = 'extension';
-export default extensionPackage;
+export default async function () {
+	const { name, intro, ...otherInfo } = await lib.init.promises.json(`${lib.assetURL}extension/活动武将/info.json`);
+	extensionPackage.package = {
+		...otherInfo,
+		intro: [
+			'新人制作扩展，希望大家支持。',
+			'新人技术不足，希望大家包涵。',
+			'<a href="https://github.com/HuoDong-Update-Organization/HuoDong-update">点击前往活动武将Github仓库</a>',
+			'<li>欢迎大家进群支持活动武将',
+			//入群二维码图片
+			'<img style=width:238px src=' + lib.assetURL + 'extension/活动武将/HuoDong_QQ.png>',
+		].join('<br>'),
+	};
+	return extensionPackage;
+};
