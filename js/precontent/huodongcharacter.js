8b41fb6dbd2ca0abae44df2ada44d9b4a0963817
mengxinzxz <2223529500@qq.com>
2025-11-17 03:14:21 +0800

    风晨图片，暂时unseen
    风晨等牢戏写完了再放出来

--- js/precontent/huodongcharacter.js
+++ js/precontent/huodongcharacter.js
@@ -68,7 +68,7 @@ const packs = function () {
             bilibili_shuijiaobuboli: ['female', 'key', 3, ['bilibili_qicai', 'bilibili_jizhi', 'bilibili_fengliang', 'bilibili_guiyin'], ['clan:宿舍群|活动群', 'name:黄|月英']],
             bilibili_kuailiangkuaiyue: ['male', 'qun', 4, ['bilibili_chouhua'], ['character:kuailiangkuaiyue']],
             bilibili_wuzhuwanshui: ['male', 'key', 3, ['bilibili_diaowen', 'bilibili_banyun'], ['clan:肘击群', 'name:猪|大将军']],
-            bilibili_murufengchen: ['male', 'key', 3, [], ['clan:肘击群|活动群', 'name:风|晨']],
+            bilibili_murufengchen: ['double', 'key', 3, [], ['unseen', 'clan:肘击群|活动群', 'name:风|晨']],
             bilibili_diandian: ['female', 'key', 3, [], ['clan:宿舍群|肘击群|活动群', 'name:永雏|塔菲']],
             //千里走单骑
             DJ_caiyang: ['male', 'qun', 1, ['yinka', 'zhuixi'], ['character:caiyang']],
@@ -12065,7 +12065,7 @@ const packs = function () {
             bilibili_diaowen_info: `①每轮开始时，你将牌堆顶三张牌称为“矢”置于武将牌上。②每轮结束时，你可以任意分配“矢”给场上本轮未使用过此“矢”牌面的牌的角色并令这些角色获得一张${get.poptip('bilibili_shi')}。③一名角色使用牌后，你移去武将牌上与此牌牌名相同的“矢”，然后将牌堆顶X张牌称为“矢”置于武将牌上（X为本回合发动此分支的次数）。`,
             bilibili_banyun: '搬运',
             bilibili_banyun_info: '每回合限一次，你可以使用一张“矢”，然后你将一名角色的一张牌称为“矢”置于武将牌上。',
-            bilibili_banyun_append: '<span style="font-family:yuanli">搬史纲要：<br><li>搬出节奏，搬出风采<br><li>史之不尽，赤之不竭</span>',
+            bilibili_banyun_append: '<span style="font-family:yuanli">长史曰：<br>“史之不尽，赤之不竭。<br>搬出节奏，搬出风采。”</span>',
             bilibili_murufengchen: '沐如风晨',
             bilibili_diandian: '点点',
         },
