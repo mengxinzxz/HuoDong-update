016999042512961242921d3cfa617dfb3831a0bf
xizifu <2238983941@qq.com>
2025-12-15 11:36:46 +0800

    牢美一技能

--- js/precontent/huodongcharacter.js
+++ js/precontent/huodongcharacter.js
@@ -60,7 +60,7 @@ const packs = function () {
             bilibili_diandian: ['female', 'key', 3, ['bilibili_siyu', 'bilibili_tamen'], ['clan:肘击群|活动群', 'name:永雏|塔菲']],
             bilibili_simayi: ['male', 'shen', 3, ['reguicai', 'fangzhu', 'rewansha', 'rezhiheng', 'rejizhi'], ['wei', 'character:shen_simayi', ...['tempname', 'die'].map(i => `${i}:new_simayi`)]],
             old_dongxie: ['female', 'qun', 4, ['juntun', 'jiaojie'], ['die:dongxie']],
-            bilibili_longjiuzhen: ['female', 'key', 4, ['bilibili_linglai', 'bilibili_yongtan'], ['clan:宿舍群|肘击群|活动群', 'name:null|美']],
+            bilibili_longjiuzhen: ['female', 'key', 4, ['bilibili_xingcan','bilibili_linglai', 'bilibili_yongtan'], ['clan:宿舍群|肘击群|活动群', 'name:null|美']],
             bilibili_gaowang: ['male', 'qun', 3, ['scsanruo', 'gaowangdead'], ['character:scs_gaowang']],
             //双面武将--正面
             bilibili_wangtao: ['female', 'shu', 3, ['huguan', 'yaopei', 'dualside'], ['dualside:bilibili_x_wangyue', 'character:wangtao', 'die:wangtao']],
@@ -11896,6 +11896,52 @@ const packs = function () {
                 },
             },
             //龙九帧
+            bilibili_xingcan: {
+                trigger: { player: ['phaseZhunbeiBegin', 'phaseJieshuBegin'] },
+                async cost(event, trigger, player) {
+                    const { result } = await player.chooseButton([
+                        get.prompt2(event.skill),
+                        [lib.suit.map(i => ['', '', `lukai_${i}`]), 'vcard'],
+                    ]).set('ai', button => {
+                        const player = get.player();
+                        const suit = button.link[2].slice(6);
+                        return Math.random();
+                    });
+                    event.result = {
+                        bool: result?.bool,
+                        cost_data: result?.links,
+                    }
+                },
+                async content(event, trigger, player) {
+                    const suit = event.cost_data[0][2].slice(6);
+                    player.popup(get.translation(suit));
+                    game.log(player, '声明了', '#y' + get.translation(suit));
+                    const next = player.judge(card => {
+                        if (get.suit(card) === get.event('suitx')) {
+                            return 1.5;
+                        }
+                        return -1.5;
+                    });
+                    next.judge2 = result => result.bool;
+                    next.set('suitx', suit);
+                    const { result } = await next;
+                    if (suit === result?.suit && game.hasPlayer(current => current != player)) {
+                        const { result } = await player.chooseTarget('咏叹：你可以令一名其他角色翻面', lib.filter.notMe).set('ai', target => {
+                            const player = get.player();
+                            const att = get.attitude(player, target);
+                            if (target.hasSkillTag('noturn')) return 0;
+                            if (att < 0 && target.isTurnedOver()) return 0;
+                            if (att > 2 && target.isTurnedOver()) return att * 10;
+                            return -att;
+                        });
+                        if (result?.targets?.length) {
+                            const [target] = result.targets;
+                            player.line(target);
+                            await target.turnOver();
+                        }
+                    }
+                },
+            },
             bilibili_linglai: {
                 trigger: { global: ['loseAfter', 'loseAsyncAfter', 'cardsDiscardAfter'] },
                 filter(event, player) {
@@ -12645,7 +12691,9 @@ const packs = function () {
             old_dongxie: '列传董翓',
             old_dongxie_ab: '董翓',
             bilibili_longjiuzhen: '龙九帧',
-            bilibili_linglai: '灵赖',
+            bilibili_xingcan: '星璨',
+            bilibili_xingcan_info: '准备阶段和结束阶段，你可以选择一种花色进行判定，若判定结果包含你选择的花色，你可以令一名其他角色翻面。',
+            bilibili_linglai: '灵籁',
             bilibili_linglai_info: '一名角色的出牌阶段开始时，你可以观看其手牌并选择其中至多两张牌。这些牌因/不因使用进入弃牌堆后，你摸两/一张牌。',
             bilibili_yongtan: '咏叹',
             bilibili_yongtan_info: '当你于出牌阶段使用牌结算完毕后，你可以选择一个点数并令一名其他角色进行判定，若判定结果与选择的点数相同，则其失去所有体力。',
